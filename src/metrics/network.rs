601842c1125316c3b6e37c1085a2c276f0437dd0
matchalatte1200 <tomo20030217@gmail.com>
2026-09-10 17:35:15 +0900

    fixed Networks

--- src/metrics/network.rs
+++ src/metrics/network.rs
@@ -1,5 +1,8 @@
 use sysinfo::Networks;
 
-pub fn get_network_usage(networks: &sysinfo::Networks) -> f64 {
-
+pub fn get_network_usage(networks: &Networks) -> u64 {
+    networks
+    .iter()
+    .map(|(_,network)| network.received()+ network.transmitted())
+    .sum()
 }
